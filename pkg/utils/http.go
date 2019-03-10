01f6403b6586a4c1fc4bc50de4ef9a33dde7c062
Antoine Baché <antoine.bache@epitech.eu>
2019-03-10 13:08:57 -0700

    Added HTTP tests

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -8,17 +8,21 @@ import (
 	"strconv"
 )
 
+func handleSDP(sdpChan chan string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		fmt.Fprintf(w, "done")
+		sdpChan <- string(body)
+	}
+}
+
 // HTTPSDPServer starts a HTTP Server that consumes SDPs
 func HTTPSDPServer() chan string {
 	port := flag.Int("port", 8080, "http server port")
 	flag.Parse()
 
 	sdpChan := make(chan string)
-	http.HandleFunc("/sdp", func(w http.ResponseWriter, r *http.Request) {
-		body, _ := ioutil.ReadAll(r.Body)
-		fmt.Fprintf(w, "done")
-		sdpChan <- string(body)
-	})
+	http.HandleFunc("/sdp", handleSDP(sdpChan))
 
 	go func() {
 		err := http.ListenAndServe(":"+strconv.Itoa(*port), nil)
---
+++ pkg/utils/http_test.go
@@ -0,0 +1,66 @@
+package utils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HTTPHandler(t *testing.T) {
+	assert := assert.New(t)
+	stream := &bytes.Buffer{}
+
+	sdpChan := make(chan string)
+	handler := handleSDP(sdpChan)
+
+	done := make(chan struct{})
+	go func() {
+		msg := "Hello\n"
+		_, err := stream.WriteString(msg)
+		assert.Nil(err)
+		res := <-sdpChan
+		assert.Equal(msg, res)
+		close(done)
+	}()
+
+	req := httptest.NewRequest("POST", "http://localhost:8080/sdp", stream)
+	w := httptest.NewRecorder()
+	handler(w, req)
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(200, resp.StatusCode)
+	assert.Equal("done", string(body))
+
+	<-done
+}
+
+func Test_HTTP(t *testing.T) {
+	assert := assert.New(t)
+
+	sdpChan := HTTPSDPServer()
+
+	msg := "Hello\n"
+	go func() {
+		stream := &bytes.Buffer{}
+		_, err := stream.WriteString(msg)
+
+		client := &http.Client{}
+
+		req, err := http.NewRequest("POST", "http://localhost:8080/sdp", stream)
+		assert.Nil(err)
+		resp, err := client.Do(req)
+		assert.Nil(err)
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		assert.Equal(200, resp.StatusCode)
+		assert.Equal("done", string(body))
+	}()
+
+	sdp := <-sdpChan
+	assert.Equal(msg, sdp)
+}
