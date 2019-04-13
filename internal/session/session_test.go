6fb075b468575d9b78bc8315094bd87941528121
Antoine Baché <antoine.bache@epitech.eu>
2019-04-12 19:43:35 -0700

    Fixed tests

--- internal/session/session_test.go
+++ internal/session/session_test.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,11 +15,20 @@ func Test_New(t *testing.T) {
 	input := bufio.NewReader(&bytes.Buffer{})
 	output := bufio.NewWriter(&bytes.Buffer{})
 
-	sess := New(nil, nil)
+	sess := New(nil, nil, "")
 	assert.Equal(os.Stdin, sess.sdpInput)
 	assert.Equal(os.Stdout, sess.sdpOutput)
+	assert.Equal(1, len(sess.stunServers))
+	assert.Equal("stun:stun.l.google.com:19302", sess.stunServers[0])
 
-	sess = New(input, output)
+	sess = New(input, output, "test:123")
 	assert.Equal(input, sess.sdpInput)
 	assert.Equal(output, sess.sdpOutput)
+	assert.Equal(1, len(sess.stunServers))
+	assert.Equal(true, strings.HasPrefix(sess.stunServers[0], "stun:"))
+	arr := strings.Split(sess.stunServers[0], ":")
+	assert.Equal(3, len(arr))
+	assert.Equal("stun", arr[0])
+	assert.Equal("test", arr[1])
+	assert.Equal("123", arr[2])
 }
